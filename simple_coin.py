04f15a3452e54cf8b2fc72ab7eeaceed80d07193
fyrezx <chinzhixian@gmail.com>
2018-08-31 23:47:59 +0800

    Challenge

--- simple_coin.py
+++ simple_coin.py
@@ -9,13 +9,9 @@ def generate_blocks(number_of_blocks):
 
     Given a number of blocks return the output after x amount of block as a string. Input blocks are EMPTY
     """
-    simple_chain = [Blockchain.genesis()]
-    parent_block = simple_chain[0]
+    
 
-    for i in range(number_of_blocks):
-        child_block = Blockchain.mine(parent_block)
-        simple_chain.append(child_block)
-        parent_block = child_block
+    ### YOUR CODE HERE
 
 
-    return child_block.hash
+    
