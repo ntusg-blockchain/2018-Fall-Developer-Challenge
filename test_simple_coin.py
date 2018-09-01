7b38a03dddf905db14f00a8c5f003d2a4b79c478
AlexXiong97 <veralexxiong@outlook.com>
2018-09-01 19:42:31 +0800

    remove timestamp for consistent testing result, add __str__ member for better dev log. fix all pylint error

--- test_simple_coin.py
+++ test_simple_coin.py
@@ -6,10 +6,10 @@ class TestMerkleProof(unittest.TestCase):
     def test_five_blocks(self):
         """Test that the blockchain is able to generate 5 blcoks with the 5th block containing the correct hash
         """
-        self.assertEqual('5cab08c8154259290d863e90847c4bf6f7ed9133aaab387a2ddf90a34f8f1a22', generate_blocks(5))
+        self.assertEqual('9400b8b82ca50e77cb017aa910f5c7a31541b2c0a95a00fba41dec6da7e18da7', generate_blocks(5))
 
     def test_ten_blocks(self):
         """Same Test but with 10 blocks
         """
-        self.assertEqual('02e8649e4e5aa2214ca983067c29e8fe2b065515b8354a39ed25f6b493803091', generate_blocks(10))
+        self.assertEqual('ac328102ec4df7f81e5c6f9701ccd7d80d21c94fe716d40061562f1ebc967568', generate_blocks(10))
         
